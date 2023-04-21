41df2c31cc8cf365092724cfddf9c84427f66601
Vishrutha Abbaiah <vishruthaabbaiah@Vishruthas-MacBook-Pro.local>
2023-04-20 21:16:28 -0400

    Decks and cards

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -2,6 +2,10 @@ import * as appDao from './app-dao.js';
 
 const AppController = (app, db) => {
     app.get('/api/languages/all', (req, res) => getAllSupportedLanguages(req, res, db))
+    app.get('/api/languages/decks/:id', (req, res) => getAllDecksLanguages(req, res, db))
+    app.get('/api/languages/posts/:id', (req, res) => getAllPostsForLanguages(req, res, db))
+    app.post('/api/languages/posts/:id/new', (req, res) => createPostForLanguage(req, res, db))
+    app.get('/api/languages/decks/:id/cards', (req, res) => getAllCardsForDeck(req, res, db))
 };
 
 
@@ -16,5 +20,61 @@ const getAllSupportedLanguages = async (req, res, db) => {
         res.sendStatus(500);
     }
 }
+
+
+//Get all decks for Language
+const getAllDecksLanguages = async (req, res, db) => {
+    try {
+        const language_id = req.params['id']
+        const result = await appDao.findAllDecksForLanguage(db, {language_id} );
+        res.json(result);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+//Get all posts for Language
+const getAllPostsForLanguages = async (req, res, db) => {
+    try {
+        const language_id = req.params['id']
+        const result = await appDao.getAllPostsForLanguages(db, {language_id} );
+        res.json(result);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+//Get all posts for Language
+const createPostForLanguage = async (req, res, db) => {
+    try {
+        const language_id = req.params['id']
+        const requestBody = {...req.body,language_id}
+        const result = await appDao.createPostForLanguage(db, requestBody );
+        res.json(result);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+//Get all posts for Language
+const getAllCardsForDeck = async (req, res, db) => {
+    try {
+        const deck_id = req.params['id']
+        const result = await appDao.getAllCardsForDeck(db, {deck_id} );
+        res.json(result);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+
 
 export default AppController;
--- controllers/app-dao.js
+++ controllers/app-dao.js
@@ -7,3 +7,47 @@ export const findAllLanguages = async (db, requestBody) => {
         });
     });
 };
+
+//Return all decks for Language
+export const findAllDecksForLanguage = async (db, requestBody) => {
+    const {language_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL find_all_decks_by_language( ? )', [language_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0]);
+        });
+    });
+};
+
+//Return all decks for Language
+export const getAllPostsForLanguages = async (db, requestBody) => {
+    const {language_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL find_all_posts_by_language( ? )', [language_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0]);
+        });
+    });
+};
+
+//Create a new post for Language
+export const createPostForLanguage = async (db, requestBody) => {
+    const {user_id, content, language_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL create_language_exchange_post( ?, ? , ? )', [user_id,content,language_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[1][0]);
+        });
+    });
+};
+
+//Return all cards for deck
+export const getAllCardsForDeck = async (db, requestBody) => {
+    const {deck_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL find_all_cards_by_deck( ? )', [deck_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0]);
+        });
+    });
+};
