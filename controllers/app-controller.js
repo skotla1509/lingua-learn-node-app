fcf48621d9a573b0aee6a04ab4597e45c9a6833b
skotla1509 <sindhukotla1509@gmail.com>
2023-04-21 04:04:38 -0400

    feedback, favorites

--- controllers/app-controller.js
+++ controllers/app-controller.js
@@ -2,12 +2,18 @@ import * as appDao from './app-dao.js';
 
 const AppController = (app, db) => {
     app.get('/api/languages/all', (req, res) => getAllSupportedLanguages(req, res, db))
+    app.post('/api/languages/:id/learn', (req, res) => addLanguageLearning(req, res, db))
     app.get('/api/languages/decks/:id', (req, res) => getAllDecksLanguages(req, res, db))
     app.get('/api/languages/posts/:id', (req, res) => getAllPostsForLanguages(req, res, db))
     app.post('/api/languages/posts/:id/new', (req, res) => createPostForLanguage(req, res, db))
     app.get('/api/languages/decks/:id/cards', (req, res) => getAllCardsForDeck(req, res, db))
     app.get('/api/languages/decks/:id/questions', (req, res) => getAllPracticeQuestionsForDeck(req, res, db))
     app.post('/api/languages/decks/:id/practice', (req, res) => createPracticeHistoryForLanguage(req, res, db))
+    app.post('/api/languages/decks/:id/user/:uid/favorite', (req, res) => markUnmarkDeckAsFavoriteForUser(req, res, db))
+    app.get('/api/languages/decks/:id/user/:uid/favorite', (req, res) => checkFavoriteDeckForUser(req, res, db))
+    app.get('/api/languages/decks/:id/feedback', (req, res) => getFeedbackForDeck(req, res, db))
+    app.get('/api/languages/decks/:id/feedback/average', (req, res) => getAverageFeedbackForDeck(req, res, db))
+    app.post('/api/languages/decks/:id/feedback', (req, res) => addNewFeedbackForDeck(req, res, db))
 };
 
 
@@ -15,6 +21,19 @@ const AppController = (app, db) => {
 const getAllSupportedLanguages = async (req, res, db) => {
     try {
         const result = await appDao.findAllLanguages(db, {});
+        res.json(result);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+const addLanguageLearning = async (req, res, db) => {
+    try {
+        const language_id = req.params['id']
+        const requestBody = {...req.body, language_id}
+        const result = await appDao.addLanguageLearning(db, requestBody);
         res.json(result);
     }
     catch (err) {
@@ -97,6 +116,71 @@ const createPracticeHistoryForLanguage = async (req, res, db) => {
         const requestBody = {...req.body,deck_id}
         const result = await appDao.createPracticeHistoryForLanguage(db, requestBody );
         res.json(result);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+// Mark Favorite Deck for user
+const markUnmarkDeckAsFavoriteForUser = async (req, res, db) => {
+    try {
+        const deck_id = req.params['id']
+        const user_id = req.params['uid']
+        const result = await appDao.markUnmarkDeckAsFavoriteForUser(db, {deck_id, user_id} );
+        res.sendStatus(200);
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+// Mark Favorite Deck for user
+const checkFavoriteDeckForUser = async (req, res, db) => {
+    try {
+        const deck_id = req.params['id']
+        const user_id = req.params['uid']
+        const result = await appDao.checkFavoriteDeckForUser(db, {deck_id, user_id} );
+        res.json(result)
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+const getFeedbackForDeck = async (req, res, db) => {
+    try {
+        const deck_id = req.params['id']
+        const result = await appDao.getFeedbackForDeck(db, {deck_id} );
+        res.json(result)
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+const getAverageFeedbackForDeck = async (req, res, db) => {
+    try {
+        const deck_id = req.params['id']
+        const result = await appDao.getAverageFeedbackForDeck(db, {deck_id} );
+        res.json(result)
+    }
+    catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+}
+
+const addNewFeedbackForDeck = async (req, res, db) => {
+    try {
+        const deck_id = req.params['id']
+        const requestBody = {...req.body, deck_id}
+        const result = await appDao.addNewFeedbackForDeck(db, requestBody );
+        res.json(result)
     }
     catch (err) {
         console.log(err);
--- controllers/app-dao.js
+++ controllers/app-dao.js
@@ -8,6 +8,16 @@ export const findAllLanguages = async (db, requestBody) => {
     });
 };
 
+export const addLanguageLearning = async (db, requestBody) => {
+    const {language_id, user_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL add_learning(?, ?)', [user_id, language_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0]);
+        });
+    });
+};
+
 //Return all decks for Language
 export const findAllDecksForLanguage = async (db, requestBody) => {
     const {language_id} = requestBody;
@@ -66,12 +76,63 @@ export const getAllPracticeQuestionsForDeck = async (db, requestBody) => {
 
 //Create a new entry for practice history for deck
 export const createPracticeHistoryForLanguage = async (db, requestBody) => {
-    const {user_id, deck_id, score} = requestBody;
+    const {user_id, deck_id, score_received} = requestBody;
     return new Promise((resolve, reject) => {
-        db.query('CALL create_practice_history_for_deck( ?, ? ,? )', [user_id,deck_id,score], (err, result) => {
+        db.query('CALL create_practice_history_for_deck( ?, ? ,? )', [user_id,deck_id,score_received], (err, result) => {
             if (err) reject(err);
             resolve(result[1][0]);
         });
     });
 };
 
+//Create a new entry for practice history for deck
+export const markUnmarkDeckAsFavoriteForUser = async (db, requestBody) => {
+    const {user_id, deck_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL mark_unmark_favorite_deck_for_user( ?, ? )', [user_id, deck_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0]);
+        });
+    });
+};
+
+//Create a new entry for practice history for deck
+export const checkFavoriteDeckForUser = async (db, requestBody) => {
+    const {user_id, deck_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('SELECT check_favorite(?, ?) as is_favorite', [user_id, deck_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0].is_favorite);
+        });
+    });
+};
+
+export const getFeedbackForDeck = async (db, requestBody) => {
+    const {deck_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL find_feedback_of_deck(?)', [deck_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0]);
+        });
+    });
+};
+
+export const getAverageFeedbackForDeck = async (db, requestBody) => {
+    const {deck_id} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('SELECT avg_rating_for_deck(?) as avg_rating', [deck_id], (err, result) => {
+            if (err) reject(err);
+            resolve(result[0]);
+        });
+    });
+};
+
+export const addNewFeedbackForDeck = async (db, requestBody) => {
+    const {deck_id, user_id, rating, comment} = requestBody;
+    return new Promise((resolve, reject) => {
+        db.query('CALL add_new_feedback(?, ?, ?, ?)', [user_id, deck_id, rating, comment], (err, result) => {
+            if (err) reject(err);
+            resolve(result[1][0]);
+        });
+    });
+};
